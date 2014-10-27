f1b300a509e890bf0b7a6877f99bab36f1a34860
Juan Carlos Galan <jcgalanh@gmail.com>
2014-10-26 22:36:06 -0700

    Setting up cython

--- src/fvht/_fvht.pyx
+++ src/fvht/_fvht.pyx
@@ -1,12 +1,6 @@
-#cdef extern from "fvht.h":
-#    char* say_hello_c()
-#
-## create the wrapper code
-#def say_hello():
-#    return say_hello_c()
+cdef extern from "fvht.h":
+    char* say_hello_c()
 
-cdef extern from "math.h":
-    double cos(double arg)
-
-def cos_func(arg):
-    return cos(arg)
+# create the wrapper code
+def say_hello():
+    return say_hello_c()
--- src/fvht/setup.py
+++ src/fvht/setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup, Extension
+from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 
-if __name__ == "__main__":
-    setup(
-        cmdclass={'build_ext', build_ext},
-        ext_modules=[Extension("fvht", ["_fvht.pyx"])]
-    )
+ext_modules = [
+    Extension("fvht",
+              ["_fvht.pyx", "fvht.c"],
+              libraries=["m"])  # Unix-like specific
+]
+
+setup(
+    name="Demos",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=ext_modules
+)
